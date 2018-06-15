696737d4f2eef8b77032beefc007234ffc546b42
Adam Bates <adam@adambates.ca>
2018-06-14 23:34:25 -0400

    Create EmailService.java

---
+++ com/solid/_4_interface_segregation/EmailService.java
@@ -0,0 +1,9 @@
+package com.solid._4_interface_segregation;
+
+class EmailService {
+
+    public void sendEmail(String address, String subject, String message) {
+        // send email
+        System.out.printf("Sending email to %s... \nSubject: %s\nMessage: %s", address, subject, message);
+    }
+}
