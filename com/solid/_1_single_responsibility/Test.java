79b7023b2909befb82c4a9600b191b3a312ee663
Adam Bates <adam@adambates.ca>
2018-06-15 09:38:14 -0400

    Update Test.java

--- com/solid/_1_single_responsibility/Test.java
+++ com/solid/_1_single_responsibility/Test.java
@@ -5,7 +5,10 @@ public class Test {
     public static void main(String[] args) {
         Emailer emailer = new Emailer();
 
-        Employee employee = new Employee(1L, "John", "Smith", "jsmith@example.com");    
+        Employee employee = new Employee(101L, "John", "Smith", "jsmith@example.com");
         emailer.send(employee);
+
+        Customer customer = new Customer(201L, "Jane", "Doe", "jane@janedoe.ca");
+        emailer.send(customer);
     }
 }
