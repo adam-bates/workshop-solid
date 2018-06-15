4ae18b250e3d911b8d14e2c2b5100dbc52007c35
Adam Bates <adam@adambates.ca>
2018-06-15 00:27:53 -0400

    Delete CustomerEmailer.java

--- com/solid/_3_liskov_substitution/CustomerEmailer.java
+++
@@ -1,9 +0,0 @@
-package com.solid._3_liskov_substitution;
-
-class CustomerEmailer implements Emailer <Customer> {
-  
-    public void send(Customer customer) {
-        System.out.println(String.format("Sending customer email to %s %s (%s) ...",
-            customer.getFirstName(), customer.getLastName(), customer.getEmailAddress()));
-    }
-}
