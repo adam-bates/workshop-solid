0edacb1dcc44282b60d4be3cb58da971796e5e87
Adam Bates <adam@adambates.ca>
2018-06-15 09:46:36 -0400

    Update Emailer.java

--- com/solid/_1_single_responsibility/Emailer.java
+++ com/solid/_1_single_responsibility/Emailer.java
@@ -12,15 +12,15 @@ class Emailer {
         if (emailable instanceof Employee)
             emailService.sendEmail(
                 emailable.getEmailAddress(),
-                String.format("%s %s", emailble.getFirstName(), emailable.getLastName()),
+                String.format("%s %s", emailable.getFirstName(), emailable.getLastName()),
                 String.format("%s,\nWe wanted to say thanks for all of the hard work you do for our company!", emailable.getFirstName())
             );
       
         else if (emailable instanceof Customer)
             emailService.sendEmail(
                 emailable.getEmailAddress(),
-                String.format("Hello, %s %s!", emailble.getFirstName(), emailble.getLastName()),
-                String.format("%s,\nThanks for being a loyal customer!", emailble.getFirstName())
+                String.format("Hello, %s %s!", emailable.getFirstName(), emailable.getLastName()),
+                String.format("%s,\nThanks for being a loyal customer!", emailable.getFirstName())
             );
       
         else throw new IllegalArgumentException();
