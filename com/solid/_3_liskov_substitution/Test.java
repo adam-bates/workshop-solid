fda81e58aba42173d0523e45f18f6af20a3c0b55
Adam Bates <adam@adambates.ca>
2018-06-15 10:59:19 -0400

    Update Test.java

--- com/solid/_3_liskov_substitution/Test.java
+++ com/solid/_3_liskov_substitution/Test.java
@@ -5,10 +5,12 @@ class Test {
     public static void main(String[] args) {
 
         Employee employee = new Employee(101L, "John", "Smith", "jsmith@example.com");
-        employee.save().refresh();
+        if (employee.save())
+            employee.refresh();
 
         Customer customer = new Customer(201L, "Jane", "Doe", "jane@janedoe.ca");
-        customer.save().refresh();
+        if (customer.save())
+            customer.refresh();
       
         new Config(); // No more save method available!
     }
