14acafe7ce86be5b7e3b69b6d3db732ad168048a
Adam Bates <adam@adambates.ca>
2018-06-14 21:46:56 -0400

    Update Emailer.java

--- com/solid/_1_single_responsibility/Emailer.java
+++ com/solid/_1_single_responsibility/Emailer.java
@@ -1,10 +1,17 @@
 package com.solid._1_single_responsibility;
 
-class EmployeeEmailer {
+class Emailer {
     
-    public void send(Employee employee) {
-        // send email to employee
-        System.out.println(String.format("Sending email to %s %s (%s) ...",
-            employee.getFirstName(), employee.getLastName(), employee.getEmailAddress()));
+    public void send(Emailable emailable) {
+        if (emailable instanceof Employee)
+            System.out.println(String.format("Sending employee email to %s %s (%s) ...",
+                emailable.getFirstName(), emailable.getLastName(), emailable.getEmailAddress()));
+      
+        else if (emailable instanceof Customer)
+            System.out.println(String.format("Sending customer email to %s %s (%s) ...",
+                emailable.getFirstName(), emailable.getLastName(), emailable.getEmailAddress()));
+      
+        else System.out.println(String.format("Sending generic email to %s %s (%s) ...",
+            emailable.getFirstName(), emailable.getLastName(), emailable.getEmailAddress()));
     }
 }
