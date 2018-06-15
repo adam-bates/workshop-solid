14f4fcb5fb07e51a9b74162c967c8f5b9d2ee1f4
Adam Bates <adam@adambates.ca>
2018-06-14 23:42:24 -0400

    Update EmployeeEmailer.java

--- com/solid/_4_interface_segregation/EmployeeEmailer.java
+++ com/solid/_4_interface_segregation/EmployeeEmailer.java
@@ -1,9 +1,18 @@
 package com.solid._4_interface_segregation;
 
 class EmployeeEmailer implements Emailer <Employee> {
-    
+  
+    private AwesomeEmailService emailService;
+  
+    public EmployeeEmailer(AwesomeEmailService emailService) {
+        this.emailService = emailService;
+    }
+  
     public void send(Employee employee) {
-        System.out.println(String.format("Sending employee email to %s %s (%s) ...",
-            employee.getFirstName(), employee.getLastName(), employee.getEmailAddress()));
+        emailService.sendEmail(
+            employee.getEmailAddress(),
+            String.format("%s %s", employee.getFirstName(), employee.getLastName()),
+            String.format("%s,\nWe wanted to say thanks for all of the hard work you do for our company!", employee.getFirstName())
+        );
     }
 }
