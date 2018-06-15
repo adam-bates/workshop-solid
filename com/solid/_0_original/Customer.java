aa659629d75e52017bbfefb440cced90ff5e0df9
Adam Bates <adam@adambates.ca>
2018-06-14 20:53:37 -0400

    Create Customer.java

---
+++ com/solid/_0_original/Customer.java
@@ -0,0 +1,34 @@
+package com.solid._0_original;
+
+class Customer {
+
+    // attributes
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    
+    // constructor
+    public Customer(long id, String firstName, String lastName, String emailAddress) {
+        setId(id);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmailAddress(emailAddress);
+    }
+
+    // accessors
+    public long getId() { return id; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmailAddress() { return emailAddress; }
+    
+    // mutators
+    public void setId(long id) { this.id = id; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setEmailAddress(String emailAddress) { this.emailAddress = emailAddress; }
+  
+    public boolean save() {
+        // Save to customer db
+        return true;
+    }
