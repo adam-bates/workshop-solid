2cda82268d39c34e8bfad9fb16b9e5765211ec01
Adam Bates <adam@adambates.ca>
2018-06-14 23:50:45 -0400

    Create Config.java

---
+++ com/solid/_5_dependency_inversion/Config.java
@@ -0,0 +1,22 @@
+package com.solid._5_dependency_inversion;
+
+final class Config extends Model {
+
+    private final String url;
+    private final String token;
+    private final int port;
+    
+    public Config() {
+    
+        // load config values from env...
+        
+        // ... until then, stub values
+        this.url = "example.com";
+        this.token = "abc123";
+        this.port = 1234;
+    }
+    
+    public String getUrl() { return url; }
+    public String getToken() { return token; }
+    public int getPort() { return port; }
+}
