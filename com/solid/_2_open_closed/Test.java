af7f5e5aaeb4014a59473d471dd8e6fe466c2f99
Adam Bates <adam@adambates.ca>
2018-06-15 09:56:37 -0400

    Update Test.java

--- com/solid/_2_open_closed/Test.java
+++ com/solid/_2_open_closed/Test.java
@@ -3,9 +3,16 @@ package com.solid._2_open_closed;
 public class Test {
   
     public static void main(String[] args) {
-        Emailer<Employee> emailer = new EmployeeEmailer();
+      
+        Emailer<Employee> empEmailer = new EmployeeEmailer();
+        Emailer<Customer> custEmailer = new CustomerEmailer();
 
-        Employee employee = new Employee(1L, "John", "Smith", "jsmith@example.com");    
-        emailer.send(employee);
+        Employee employee = new Employee(101L, "John", "Smith", "jsmith@example.com");
+        if (employee.save())
+            empEmailer.sendTo(employee);
+
+        Customer customer = new Customer(201L, "Jane", "Doe", "jane@janedoe.ca");
+        if (customer.save())
+            custEmailer.sendTo(customer);
     }
 }
