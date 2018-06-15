ea8fbeae29a18782713b11f68f785f56af50fbfb
Adam Bates <adam@adambates.ca>
2018-06-15 10:20:09 -0400

    Update GUIObject.java

--- com/solid/_4_interface_segregation/GUIObject.java
+++ com/solid/_4_interface_segregation/GUIObject.java
@@ -3,7 +3,8 @@ package com.solid._4_interface_segregation;
 class GUIObject implements Refreshable <GUIObject> {
 
     public GUIObject refresh() {
-        // refresh
+        System.out.println("Refershing GUI object...");
+        // refresh GUI object
         return this;
     }
 }
