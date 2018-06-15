dcd1559c30bc3b0e9639e1a89d077a27936ee348
Adam Bates <adam@adambates.ca>
2018-06-15 00:33:46 -0400

    Update Test.java

--- com/solid/_5_dependency_inversion/Test.java
+++ com/solid/_5_dependency_inversion/Test.java
@@ -3,7 +3,7 @@ package com.solid._5_dependency_inversion;
 public class Test {
   
     public static void main(String[] args) {
-        Emailer<Employee> emailer = new EmployeeEmailer();
+        Emailer<Employee> emailer = new EmployeeEmailer(new AwesomeEmailService());
 
         Employee employee = new Employee(1L, "John", "Smith", "jsmith@example.com");
         emailer.send(employee);
