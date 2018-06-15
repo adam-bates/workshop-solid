2fb0881197f7a2eae98d39b7a572b954ac890eb4
Adam Bates <adam@adambates.ca>
2018-06-14 20:02:42 -0400

    Created original Employee class

---
+++ com/solid/original/Employee.java
@@ -0,0 +1,32 @@
+package com.solid.original;
+
+class Employee {
+
+    // attributes
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    
+    // constructor
+    public Employee(long id, String firstName, String lastName, String emailAddress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+    }
+
+    // accessors
+    public long getId() { return this.id; }
+    public String getFirstName() { return this.firstName; }
+    public String getLastName() { return this.lastName; }
+    
+    // mutators
+    public void setId(long id) { this.id = id; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public void sendEmail() {
+        // send email to employee
+    }
+}
