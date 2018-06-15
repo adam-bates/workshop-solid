09f977e596eca900e6365881e8c7be5e92d09150
Adam Bates <adam@adambates.ca>
2018-06-15 10:11:32 -0400

    Update AwesomeEmailService.java

--- com/solid/_4_interface_segregation/AwesomeEmailService.java
+++ com/solid/_4_interface_segregation/AwesomeEmailService.java
@@ -4,6 +4,6 @@ class AwesomeEmailService implements EmailService {
 
     public void sendEmail(String address, String subject, String message) {
         // send email
-        System.out.printf("Sending email to %s... \nSubject: %s\nMessage: %s", address, subject, message);
+        System.out.printf("Sending email to %s... \nSubject: %s\nMessage: %s\n\n", address, subject, message);
     }
 }
