7e8b5ba82aee1771c971b2a0c73e7f2e92e1d22b
Adam Bates <adam@adambates.ca>
2018-06-15 10:26:07 -0400

    Update Test.java

--- com/solid/_4_interface_segregation/Test.java
+++ com/solid/_4_interface_segregation/Test.java
@@ -3,9 +3,18 @@ package com.solid._4_interface_segregation;
 public class Test {
   
     public static void main(String[] args) {
-        Emailer<Employee> emailer = new EmployeeEmailer();
+        Emailer<Employee> empEmailer = new EmployeeEmailer();
+        Emailer<Customer> custEmailer = new CustomerEmailer();
 
-        Employee employee = new Employee(1L, "John", "Smith", "jsmith@example.com");
-        emailer.send(employee);
+        Employee employee = new Employee(101L, "John", "Smith", "jsmith@example.com");
+        if (employee.save())
+            empEmailer.sendTo(employee.refresh());
+
+        Customer customer = new Customer(201L, "Jane", "Doe", "jane@janedoe.ca");
+        if (customer.save())
+            custEmailer.sendTo(customer.refresh());
+      
+        GUIObject guiObject = new GUIObject();
+        for (int i = 0; i < 10; i++) guiObject.refresh();
     }
 }
