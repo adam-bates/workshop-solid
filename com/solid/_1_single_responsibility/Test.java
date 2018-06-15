456c52e122562dd083acd994805b605c0540cc9b
Adam Bates <adam@adambates.ca>
2018-06-14 21:50:18 -0400

    Update Test.java

--- com/solid/_1_single_responsibility/Test.java
+++ com/solid/_1_single_responsibility/Test.java
@@ -3,7 +3,7 @@ pacakge com.solid._1_single_responsibility;
 public class Test {
   
     public static void main(String[] args) {
-        EmployeeEmailer emailer = new EmployeeEmailer();
+        Emailer emailer = new Emailer();
 
         Employee employee = new Employee(1L, "John", "Smith", "jsmith@example.com");    
         emailer.send(employee);
