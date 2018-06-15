4aeaa379f8fb438b8a4ed7138763bda643203984
Adam Bates <adam@adambates.ca>
2018-06-15 00:28:02 -0400

    Delete EmployeeEmailer.java

--- com/solid/_3_liskov_substitution/EmployeeEmailer.java
+++
@@ -1,9 +0,0 @@
-package com.solid._3_liskov_substitution;
-
-class EmployeeEmailer implements Emailer <Employee> {
-    
-    public void send(Employee employee) {
-        System.out.println(String.format("Sending employee email to %s %s (%s) ...",
-            employee.getFirstName(), employee.getLastName(), employee.getEmailAddress()));
-    }
-}
