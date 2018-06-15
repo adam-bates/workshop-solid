594620fc859f67df6b75c82316f09302d958a415
Adam Bates <adam@adambates.ca>
2018-06-15 00:08:43 -0400

    Create AwesomeEmailService.java

---
+++ com/solid/_0_original/AwesomeEmailService.java
@@ -0,0 +1,9 @@
+package com.solid._0_original;
+
+class AwesomeEmailService {
+    public void sendEmail(String address, String subject, String message) {
+        // send email
+        System.out.printf("Sending email to %s... \nSubject: %s\nMessage: %s", address, subject, message);
+    }
+}
+        
