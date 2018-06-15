d8a0af299f490dae58a25ffbdd618b682550067a
Adam Bates <adam@adambates.ca>
2018-06-14 23:49:58 -0400

    Create AwesomeEmailService.java

---
+++ com/solid/_5_dependency_inversion/AwesomeEmailService.java
@@ -0,0 +1,8 @@
+package com.solid._5_dependency_inversion;
+
+class AwesomeEmailService implements EmailService {
+    void sendEmail(String address, String subject, String message) {
+        // send email
+        System.out.printf("Sending email to %s... \nSubject: %s\nMessage: %s", address, subject, message);
+    }
+}
