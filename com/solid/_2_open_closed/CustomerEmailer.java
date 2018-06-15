89a31b03ce309f5444cdb9a24aaf1e5ff9079201
Adam Bates <adam@adambates.ca>
2018-06-15 00:23:55 -0400

    Update CustomerEmailer.java

--- com/solid/_2_open_closed/CustomerEmailer.java
+++ com/solid/_2_open_closed/CustomerEmailer.java
@@ -2,8 +2,17 @@ package com.solid._2_open_closed;
 
 class CustomerEmailer implements Emailer <Customer> {
   
+    private AwesomeEmailService emailService;
+    
+    public CustomerEmailer() {
+        emailService = new AwesomeEmailService();
+    }
+  
     public void send(Customer customer) {
-        System.out.println(String.format("Sending customer email to %s %s (%s) ...",
-            customer.getFirstName(), customer.getLastName(), customer.getEmailAddress()));
+        emailService.sendEmail(
+              customer.getEmailAddress(),
+              String.format("Hello, %s %s!", customer.getFirstName(), customer.getLastName()),
+              String.format("%s,\nThanks for being a loyal customer!", customer.getFirstName())
+          );
     }
 }
