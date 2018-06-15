2791ba4d6dbd6cb8b01fa9d007d0927ac8bc6f50
Adam Bates <adam@adambates.ca>
2018-06-15 09:34:17 -0400

    Update AwesomeEmailService.java

--- com/solid/_1_single_responsibility/AwesomeEmailService.java
+++ com/solid/_1_single_responsibility/AwesomeEmailService.java
@@ -3,6 +3,6 @@ package com.solid._1_single_responsibility;
 class AwesomeEmailService {
     public void sendEmail(String address, String subject, String message) {
         // send email
-        System.out.printf("Sending email to %s... \nSubject: %s\nMessage: %s", address, subject, message);
+        System.out.printf("Sending email to %s... \nSubject: %s\nMessage: %s\n\n", address, subject, message);
     }
 }
