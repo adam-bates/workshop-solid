ebe63d870f1db0834d77a465b9303f9f3c8f8765
Adam Bates <adam@adambates.ca>
2018-06-15 09:40:34 -0400

    Update Test.java

--- com/solid/_0_original/Test.java
+++ com/solid/_0_original/Test.java
@@ -4,7 +4,12 @@ public class Test {
 
     public static void main(String[] args) {
 
-        new Employee(101L, "John", "Smith", "jsmith@example.com").sendEmail();
-        new Customer(201L, "Jane", "Doe", "jane@janedoe.ca").sendEmail();
+        Employee employee = new Employee(101L, "John", "Smith", "jsmith@example.com");
+        if (employee.save())
+            employee.sendEmail();
+
+        Customer customer = new Customer(201L, "Jane", "Doe", "jane@janedoe.ca");
+        if (customer.save())
+            customer.sendEmail();
     }
 }
