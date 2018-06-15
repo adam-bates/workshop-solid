248007b36069b753ee6b7fa7cb28979a62506e81
Adam Bates <adam@adambates.ca>
2018-06-14 23:58:46 -0400

    Create Employee.java

---
+++ com/solid/_5_dependency_inversion/Employee.java
@@ -0,0 +1,40 @@
+package com.solid._5_dependency_inversion;
+
+class Employee extends DBModel <Employee> implements Emailable {
+
+    // attributes
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    
+    // constructor
+    public Employee(long id, String firstName, String lastName, String emailAddress) {
+        setId(id);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmailAddress(emailAddress);
+    }
+
+    // accessors
+    public long getId() { return id; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmailAddress() { return emailAddress; }
+    
+    // mutators
+    public void setId(long id) { this.id = id; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setEmailAddress(String emailAddress) { this.emailAddress = emailAddress; }
+  
+    public boolean save() {
+        // Save employee to db
+        return true;
+    }
+    
+    public Employee refresh() {
+        // refresh employee
+        return this;
+    }
+}
