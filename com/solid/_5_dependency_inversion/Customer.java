d0e95cbb25092f147958a9ef1fd7ced4e5635c4f
Adam Bates <adam@adambates.ca>
2018-06-14 23:52:25 -0400

    Create Customer.java

---
+++ com/solid/_5_dependency_inversion/Customer.java
@@ -0,0 +1,40 @@
+package com.solid._5_dependency_inversion;
+
+class Customer extends DBModel <Customer> implements Emailable {
+
+    // attributes
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    
+    // constructor
+    public Customer(long id, String firstName, String lastName, String emailAddress) {
+        setId(id);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmailAddress(emailAddress);
+    }
+
+    // accessors
+    public long getId() { return id; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmailAddress() { return emailAddress; }
+    
+    // mutators
+    public void setId(long id) { this.id = id; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setEmailAddress(String emailAddress) { this.emailAddress = emailAddress; }
+  
+    public boolean save() {
+        // Save customer to db
+        return true;
+    }
+    
+    public Customer refresh() {
+        // refresh Customer
+        return this;
+    }
+}
