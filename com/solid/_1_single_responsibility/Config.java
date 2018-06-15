725ed2e2f5b8d48ea7049a32c669aba36bbd69a9
Adam Bates <adam@adambates.ca>
2018-06-14 22:41:04 -0400

    Create Config.java

---
+++ com/solid/_1_single_responsibility/Config.java
@@ -0,0 +1,27 @@
+package com.solid._1_single_responsibility;
+
+final class Config extends Model {
+
+    private final String url;
+    private final String token;
+    private final int port;
+    
+    public Config() {
+    
+        // load config values from env...
+        
+        // ... until then, stub values
+        this.url = "example.com";
+        this.token = "abc123";
+        this.port = 1234;
+    }
+    
+    public String getUrl() { return url; }
+    public String getToken() { return token; }
+    public int getPort() { return port; }
+    
+    // Config values shouldn't be saved from application
+    public boolean save() {
+        throw new UnsupportedOperationException();
+    }
+}
