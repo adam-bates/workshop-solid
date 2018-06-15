cb310853efe6a8e1d7435b83c3e2d2e6a15e5505
Adam Bates <adam@adambates.ca>
2018-06-15 09:33:38 -0400

    Update Test.java

--- com/solid/_0_original/Test.java
+++ com/solid/_0_original/Test.java
@@ -3,7 +3,8 @@ package com.solid._0_original;
 public class Test {
 
     public static void main(String[] args) {
-        Employee employee = new Employee(1L, "John", "Smith", "jsmith@example.com");
-        employee.sendEmail();
+
+        new Employee(101L, "John", "Smith", "jsmith@example.com").sendEmail();
+        new Customer(201L, "Jane", "Doe", "jane@janedoe.ca").sendEmail();
     }
 }
