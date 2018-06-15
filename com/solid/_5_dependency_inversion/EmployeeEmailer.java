017891c392b8b4bbd2b6c4bf66b6818f6458646d
Adam Bates <adam@adambates.ca>
2018-06-15 00:14:21 -0400

    Create EmployeeEmailer.java

---
+++ com/solid/_5_dependency_inversion/EmployeeEmailer.java
@@ -0,0 +1,18 @@
+package com.solid._5_dependency_inversion;
+
+class EmployeeEmailer implements Emailer <Employee> {
+  
+    private EmailService emailService;
+    
+    public EmployeeEmailer() {
+        emailService = new AwesomeEmailService();
+    }
+  
+    public void send(Employee employee) {
+        emailService.sendEmail(
+            employee.getEmailAddress(),
+            String.format("%s %s", employee.getFirstName(), employee.getLastName()),
+            String.format("%s,\nWe wanted to say thanks for all of the hard work you do for our company!", employee.getFirstName())
+        );
+    }
+}
