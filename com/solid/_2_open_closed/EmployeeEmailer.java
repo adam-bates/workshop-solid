f537f531c66451c461f28a867dab68e1c2e8799b
Adam Bates <adam@adambates.ca>
2018-06-15 00:24:40 -0400

    Update EmployeeEmailer.java

--- com/solid/_2_open_closed/EmployeeEmailer.java
+++ com/solid/_2_open_closed/EmployeeEmailer.java
@@ -3,7 +3,10 @@ package com.solid._2_open_closed;
 class EmployeeEmailer implements Emailer <Employee> {
     
     public void send(Employee employee) {
-        System.out.println(String.format("Sending employee email to %s %s (%s) ...",
-            employee.getFirstName(), employee.getLastName(), employee.getEmailAddress()));
+        emailService.sendEmail(
+            employee.getEmailAddress(),
+            String.format("%s %s", employee.getFirstName(), employee.getLastName()),
+            String.format("%s,\nWe wanted to say thanks for all of the hard work you do for our company!", employee.getFirstName())
+        );
     }
 }
